ad8f3758c1e34997b8d8bc8629be88b5329cc958
durga-ammu <62275615+durga-ammu@users.noreply.github.com>
2020-04-11 06:30:36 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function Expand(idName) {
+  document.querySelector("#" + idName).classList.add("reveal");
+}
+
+function expand(idName) {
+  document.querySelector("#" + idName).classList.add("hidden");
+}
